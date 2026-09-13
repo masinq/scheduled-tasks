6adc590128f50e73aaeccd5e6aa437c9ccebecee
masinq <matthewsinquefield@gmail.com>
2026-09-12 21:30:24 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
 
-
-from datetime import datetime
-import pandas
-import random
+import requests
 import smtplib
-import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+from appier import email_parts
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+MY_EMAIL = "matthewsinquefield@gmail.com"
+PASSWORD = "dlrn wbqp tvpx ayjg"
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+
+
+
+api_key = '1cb689045543cf590a9d15213b27f969'
+lat = 33.28
+lon = -81.37
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast?"
+weather_params = {
+    'lat':lat
+    ,'lon':lon,
+    'appid':api_key
+    ,'cnt':4
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+weather_data = response.json()
+message = "Bring an umbrella"
+
+
+
+
+def is_it_raining():
+    rain_list = []
+    for hour_data in weather_data['list']:
+        condition = hour_data['weather'][0]['id']
+        if condition < 700:
+            rain_list.append(condition)
+    if len(rain_list) > 0:
+        return True
+    else:
+        return False
+
+if is_it_raining():
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(user=MY_EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs='matthewsinquefield@gmail.com',
+                            msg=f"Subject:Rain Ahead!\n\n{message}"
+                            )
